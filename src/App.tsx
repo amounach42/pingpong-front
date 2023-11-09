cfbe660f9e49cf85f01cb1c70bdb5eda0cef1037
amounach <mounach-@hotmail.com>
2023-11-09 19:10:52 +0100

    Added scoring logic for Players

--- src/App.tsx
+++ src/App.tsx
@@ -13,55 +13,156 @@ function App() {
       Bodies = Matter.Bodies,
       Composite = Matter.Composite;
 
-      // create an engine
-      var engine = Engine.create({
-        gravity: {x: 0, y:0},
-  positionIterations: 10, // Increase the number of position iterations
-  velocityIterations: 8,  // Increase the number of velocity iterations
-});
-       var engine = Engine.create({gravity: {x: 0, y: 0}});
-      Matter.Events.on(engine, "collisionStart", (event) => {
-        event.pairs.forEach((pair) => {
-          const bodyA = pair.bodyA;
-          const bodyB = pair.bodyB;
-          // console.log("Collision between:", bodyA, bodyB);
-          // console.log("Ball velocity:", ball.velocity);
+    // create an engine
+    var engine = Engine.create({
+      gravity: { x: 0, y: 0 },
+      positionIterations: 10, // Increase the number of position iterations
+      velocityIterations: 8,  // Increase the number of velocity iterations
+    });
+    var engine = Engine.create({ gravity: { x: 0, y: 0 } });
+    Matter.Events.on(engine, "collisionStart", (event) => {
+      event.pairs.forEach((pair) => {
+        const bodyA = pair.bodyA;
+        const bodyB = pair.bodyB;
+        // console.log("Collision between:", bodyA, bodyB);
+        // console.log("Ball velocity:", ball.velocity);
 
 
-        });
       });
+    });
 
     // create a renderer
     var render = Render.create({
       element: ref.current || document.body,
       engine: engine,
-      options:{
+      options: {
         background: '#ffffff',
         width: 600,
         height: 800,
         wireframes: false,
       }
     });
-  
-    // create two boxes and a ground
+    // --------------------------- create two boxes and a ground ---------------------------
     var bottom = Bodies.rectangle(300, 800, 600, 10, { isStatic: true });
-    var top = Bodies.rectangle(300, 0, 600, 10, {isStatic: true});
-    var right = Bodies.rectangle(600, 400, 10, 800, {isStatic: true})
-    var left = Bodies.rectangle(0, 400, 10, 800, {isStatic: true})
-    var p1 = Bodies.rectangle(300, 770, 120, 20, { isStatic: true,
-      chamfer: { radius: 10 }, });
-    var p2 = Bodies.rectangle(300, 30, 120, 20, { isStatic: true, 
+    var top = Bodies.rectangle(300, 0, 600, 10, { isStatic: true });
+    var right = Bodies.rectangle(600, 400, 10, 800, { isStatic: true })
+    var left = Bodies.rectangle(0, 400, 10, 800, {
+      isStatic: true,
+    })
+    var p1 = Bodies.rectangle(300, 770, 120, 20, {
+      isStatic: true,
       chamfer: { radius: 10 },
-    render: {
-      fillStyle: "purple",
-    } });
+    });
+    var p2 = Bodies.rectangle(300, 15, 120, 20, {
+      isStatic: true,
+      chamfer: { radius: 10 },
+      render: {
+        fillStyle: "purple",
+      }
+    });
     var ball = Bodies.circle(300, 400, 10, {
       frictionAir: 0,
       friction: 0,
-      force: {x: 1, y: 3},
+      force: { x: 1, y: 3 },
       inertia: Infinity,
       density: 0.5,
-      restitution: 1
+      restitution: 1,
+      velocity: {
+        x: 5,
+        y: -5,
+      }
+    });
+
+    
+    Matter.Events.on(engine, "collisionStart", (event) => {
+      console.log("velocity: ", ball.velocity);
+    });
+
+    Matter.Events.on(engine, "collisionStart", (event) => {
+      event.pairs.forEach((pair) => {
+        const bodyA = pair.bodyA;
+        const bodyB = pair.bodyB;
+
+        // Check if the collision involves the ball
+        if (bodyA === ball || bodyB === ball) {
+          const collisionNormal = pair.collision.normal;
+
+          // Check if the angle is less than a threshold (in radians)
+          const angleThreshold = 0.1; // Adjust this threshold as needed
+
+          if (Math.abs(collisionNormal.x) < angleThreshold) {
+            // Adjust the x-component of the velocity by a small increment
+            const sign = ball.velocity.x > 0 ? 1 : -1; // Adjust the sign based on the current direction
+            const adjustedVelocity = {
+              x: ball.velocity.x + sign * 0.5, // Adjust this increment as needed
+              y: ball.velocity.y,
+            };
+
+            // Set the adjusted velocity for the ball
+            Body.setVelocity(ball, adjustedVelocity);
+          }
+        }
+      });
+    });
+ //   ----------------------------------------------------------------------
+// Initialize scores for Player 1 and Player 2
+let scorePlayer1 = 0;
+let scorePlayer2 = 0;
+
+// Inside your update loop or event handling
+Matter.Events.on(engine, "beforeUpdate", () => {
+  // Check if the ball has crossed the first paddle (Player 1)
+  if (
+    ball.position.y < 5 &&
+    ball.velocity.y < 0 // Ensure the ball is moving upwards
+  ) {
+    // Increment Player 2's score
+    scorePlayer2++;
+
+    // Log the updated scores (you can remove these lines if you don't want to log scores)
+    console.log("BOT SCORE:", scorePlayer1);
+    console.log("Player 2 Score:", scorePlayer2);
+
+    // Reset the ball to the center
+    Body.setPosition(ball, { x: 300, y: 400 });
+
+    // Reset the ball's velocity if needed
+    Body.setVelocity(ball, { x: 5, y: -5 });
+  }
+
+  // Check if the ball has crossed the second paddle (Player 2)
+  if (
+    ball.position.y > 770 &&
+    ball.velocity.y > 0 // Ensure the ball is moving downwards
+  ) {
+    scorePlayer1++;
+
+    // Log the updated scores (you can remove these lines if you don't want to log scores)
+    console.log("BOT SCORE :", scorePlayer1);
+    console.log("Player 2 Score:", scorePlayer2);
+
+    // Reset the ball to the center
+    Body.setPosition(ball, { x: 300, y: 400 });
+
+    // Reset the ball's velocity if needed
+    Body.setVelocity(ball, { x: 5, y: -5 }); // Adjust the initial velocity as needed
+  }
+});
+    // ---------------------------------------------------------------------
+
+    // Inside your update loop or event handling
+    Matter.Events.on(engine, "beforeUpdate", () => {
+      const maxX = 600; // Adjust these values based on your world size
+      const maxY = 800;
+
+      // Check if the ball is outside the bounds
+      if (ball.position.x < 0 || ball.position.x > maxX || ball.position.y < 0 || ball.position.y > maxY) {
+        // Reset the ball to the center
+        Body.setPosition(ball, { x: 300, y: 400 });
+
+        // Reset the ball's velocity if needed
+        Body.setVelocity(ball, { x: 5, y: -5 }); // Adjust the initial velocity as needed
+      }
     });
 
     // add all of the bodies to the world
@@ -73,70 +174,34 @@ function App() {
     var runner = Runner.create();
 
     // run the engine
-    // while(1)
     Runner.run(runner, engine);
-    // Body.setPosition(p2, { x: ball.position.x, y: 10 });
+    Body.setPosition(p2, { x: ball.position.x, y: 25 });
     document.addEventListener("mousemove", (event) => {
       const mouseX = event.clientX;
-      
+
       if (render.options && render.options.width) {
         const paddleWidth = 120;
         const paddleX = Math.min(
           Math.max(mouseX - paddleWidth / 2, paddleWidth / 2),
           render.options.width - paddleWidth / 2
-          );
-          
-          // Update paddle position
-          Body.setPosition(p1, { x: paddleX, y: p1.position.y });
-        }
-      });
-      
-    Matter.Events.on(engine, "beforeUpdate", () =>{
-        Body.setPosition(p2, { x: ball.position.x, y: p2.position.y });
-    })
-    
-    Matter.Events.on(engine, "collisionStart", (event) => {
-      event.pairs.forEach((pair) => {
-        const bodyA = pair.bodyA;
-        const bodyB = pair.bodyB;
-    
-        // Check if the collision involves the ball
-        if (bodyA === ball || bodyB === ball) {
-          const collisionAngle = Math.atan2(ball.velocity.y, ball.velocity.x);
-          const collisionAngleDegrees = (collisionAngle * 180) / Math.PI;
-    
-          // Log the original collision angle in degrees
-          console.log("Original Collision Angle (Degrees):", collisionAngleDegrees);
-    
-          // Check if the angle is less than 60 degrees
-          if (collisionAngleDegrees < 60) {
-            // Increase the angle, for example, by 20 degrees
-            const newAngleDegrees = collisionAngleDegrees + 20;
-    
-            // Convert the new angle back to radians
-            const newAngle = (newAngleDegrees * Math.PI) / 180;
-    
-            // Calculate a new velocity vector with the adjusted angle
-            const newVelocity = {
-              x: Math.cos(newAngle) * Matter.Vector.magnitude(ball.velocity),
-              y: Math.sin(newAngle) * Matter.Vector.magnitude(ball.velocity),
-            };
-    
-            // Set the new velocity for the ball
-            Body.setVelocity(ball, newVelocity);
-    
-            // Log the new collision angle in degrees
-            console.log("New Collision Angle (Degrees):", newAngleDegrees);
-          }
-        }
-      });
+        );
+
+
+        // Update paddle position
+        Body.setPosition(p1, { x: paddleX, y: p1.position.y });
+      }
     });
-    
-    return () =>{
+
+
+
+    Matter.Events.on(engine, "beforeUpdate", () => {
+      Body.setPosition(p2, { x: ball.position.x, y: p2.position.y });
+    })
+    return () => {
       render.canvas.remove();
     }
   }, []);
-  return<div>
+  return <div>
     <div ref={ref}></div>
   </div>;
 }
